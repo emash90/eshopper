a470efc2bd736844db1ec6b8fd0dd3680e07a4af
edwin <edwin.macharia@gozem.co>
2022-12-18 13:31:58 +0300

    developing the home page and items card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import StoreItem from '../components/StoreItem'
+import storeItems from '../data/items.json'
 
 const Home = () => {
   return (
+    <>
     <h1>
         Home
     </h1>
+    <Row md={2} xs={1} lg={3} className="g-3" >
+        {storeItems.map((item) => (
+            <Col key={item.id}>
+                <StoreItem {...item} />
+            </Col>
+        )
+        )}
+    </Row>
+    </>
   ) 
 }
 
